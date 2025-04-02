1888eff7c7b9777c63aebff7309788a2b4f31840
origin <mineun1206@naver.com>
2025-04-02 14:27:37 +0900

    FoodCare UI

--- app/src/main/java/com/AzaAza/foodcare/MainActivity.kt
+++ app/src/main/java/com/AzaAza/foodcare/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
